14f1acd7bcb57aa83abd25c51429c9f60b4fa044
Jisan901 <jisuna901@gmail.com>
2023-03-10 17:18:36 +0600

    new

---
+++ app.py
@@ -0,0 +1,55 @@
+from PIL import ImageTk
+import tkinter as tk
+
+root = tk.Tk()
+
+root.title('Helpline')
+root.eval('tk::PlaceWindow . center')
+
+root.geometry('500x750+'+str(root.winfo_screenwidth()//2)+'+'+str(int(root.winfo_screenheight()*0.1)))
+
+frame1 = tk.Frame(root, height=350, width=500,bg="#ffffff")
+frame1.grid(row=0,column=0)
+frame1.pack_propagate(False)
+
+logo = ImageTk.PhotoImage(file='./assets/banner2.jpg')
+logo_widget = tk.Label(frame1,image=logo,bg="#ffffff")
+logo_widget.image=logo
+logo_widget.pack()
+
+
+tk.Label(frame1,
+text="All Helplines are hear",
+bg="#ffffff",
+fg="#000000",
+font=("TkMenuFont",12),
+pady=25).pack()
+
+frame2=tk.Frame(root,width=500,height=400,bg="#ffffff")
+frame2.grid(row=2,column=0)
+frame2.grid_propagate(False)
+
+tk.Button(frame2,
+text="999"
+).place(relx=0.5,rely=0.3,anchor=tk.CENTER)
+tk.Button(frame2,
+text="999"
+).place(relx=0.2,rely=0.3,anchor=tk.CENTER)
+tk.Button(frame2,
+text="999"
+).place(relx=0.8,rely=0.3,anchor=tk.CENTER)
+
+
+tk.Button(frame2,
+text="999"
+).place(relx=0.5,rely=0.6,anchor=tk.CENTER)
+tk.Button(frame2,
+text="999"
+).place(relx=0.2,rely=0.6,anchor=tk.CENTER)
+tk.Button(frame2,
+text="999"
+).place(relx=0.8,rely=0.6,anchor=tk.CENTER)
+
+
+
+root.mainloop()
