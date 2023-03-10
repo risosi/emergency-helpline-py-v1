1ca093446254b82833568c0d13533a99c9b07ecb
Jisan901 <jisuna901@gmail.com>
2023-03-10 19:48:14 +0600

    guix

--- app.py
+++ app.py
@@ -5,11 +5,13 @@ root = tk.Tk()
 
 root.title('Helpline')
 root.eval('tk::PlaceWindow . center')
-
-root.geometry('500x750+'+str(root.winfo_screenwidth()//2)+'+'+str(int(root.winfo_screenheight()*0.1)))
+root['bg']='#ffffff'
+#root.geometry('500x750+'+str(root.winfo_screenwidth()//2)+'+'+str(int(root.winfo_screenheight()*0.1)))
 
 frame1 = tk.Frame(root, height=350, width=500,bg="#ffffff")
-frame1.grid(row=0,column=0)
+frame1.place(relx=0.5,rely=0.35,anchor=tk.CENTER)
+#grid(row=0,column=0)
+#pack(side='top',fill='both')
 frame1.pack_propagate(False)
 
 logo = ImageTk.PhotoImage(file='./assets/banner2.jpg')
@@ -26,7 +28,8 @@ font=("TkMenuFont",12),
 pady=25).pack()
 
 frame2=tk.Frame(root,width=500,height=400,bg="#ffffff")
-frame2.grid(row=2,column=0)
+frame2.place(relx=0.5,rely=0.6,anchor=tk.CENTER)
+#frame2.pack(side='top',fill='both')
 frame2.grid_propagate(False)
 
 tk.Button(frame2,
